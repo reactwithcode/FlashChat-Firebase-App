ad7dc7d6e1fcb6dee40e8293d2a978797b169a13
thohariakb <thohari.akbar@gmail.com>
2018-04-05 21:31:42 +0700

    First Commit

---
+++ app/src/main/java/com/haridevapp/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,28 @@
+package com.haridevapp.flashchatnewfirebase;
+
+/**
+ * Created by HARI on 16/03/2018.
+ */
+
+public class InstantMessage {
+
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
